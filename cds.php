b12dddef5ccd32ae23978d545b008b0bee686809
aluno <aluno@LINUX-PHP-JAVA>
2016-04-15 14:25:46 -0300

    inclusão classes zend-formulario

--- cds.php
+++ cds.php
@@ -1,8 +1,14 @@
 <?php
 require('cdcol.php');
-$cds= $cdcol->fetchAll('SELECT titel, interpret FROM cds');
+/*$cds= $cdcol->fetchAll('SELECT titel, interpret FROM cds');
 foreach($cds as $cd){
 	print "<p> Título: $cd->titel </p>";
 	print"<p>Cantor: $cd->interpret </p>";
-	print "<hr>";
+	print "<hr>"; 
+
+}*/
+$cds = $cdcol->fetchAll('Select * From cds');
+foreach($cds as $cd){
+	print "<p> $cd->titel($cd-interpret) - Ações:
+	<a href=\"zend-formulario-cd.php?id=$cd-id\">editar</a> |excluir </p>";
 }
